0283bc2082f85e24720e26aa7d1273b5462a5241
jeoungneahyuk1 <jeoungneahyuk1@naver.com>
2024-04-17 16:48:53 +0900

    Add mul function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -21,4 +21,11 @@ class Control:
     def sum(self, a, b):
         return a+b
         
+    def sub(self, a, b):
+        return a-b
+    
+    def mul(self, a, b):
+        return a*b
+    
+    
     
